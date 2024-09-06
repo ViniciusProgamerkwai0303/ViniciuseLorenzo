c5db9b2d13e2b6cb473bc727de4fe656e1e77dbf
Vinicius(Karl Marx) + Lorenzo(Nicolas Tesla) <JenilsonSantossantista@gmail.com>
2024-09-06 09:01:25 -0300

    Add files via upload

---
+++ ALURA PLUS java script vinicius e lorenzo/codiguinho.js
@@ -0,0 +1,16 @@
+console.log("\nola mundo lindo :b\n"); //escreve isso
+
+const aluno1 = "vinicius";
+const aluno2 = "Lorenzo";
+
+const idadeAluno1 = 16;
+const idadeAluno2 = 16;
+
+const somaIdades = idadeAluno1 + idadeAluno2;
+
+ console.log(" a soma das idades de vinicius e lorenzo é", somaIdades+"\n");
+
+ const dupla = aluno1+ " e " +aluno2;
+
+ console.log("somos a dupla:", dupla+"\n");
+
