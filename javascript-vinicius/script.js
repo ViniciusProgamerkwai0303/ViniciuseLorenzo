b481ae93fbb4a8ea42110a0770e4b8714defdea9
Vinicius(Karl Marx) + Lorenzo(Nicolas Tesla) <JenilsonSantossantista@gmail.com>
2024-09-18 09:55:49 -0300

    Add files via upload

---
+++ javascript-vinicius/script.js
@@ -0,0 +1,23 @@
+alert("Testando o script")
+
+var nome=prompt("Qual seu nome ?")
+
+var sobrenome=prompt("Qual seu sobrenome ?")
+
+console.log("olá " + nome + " " + sobrenome + " seja bem vindo!")
+
+var idade =prompt("Qual sua idade ?")
+
+if(idade>=18 & idade<=100){
+    console.log("Mães solteiras a 5km de vocẽ")
+    var idade20 = ("Mães solteiras a 5km de vocẽ")
+}else{
+    if(idade<18){
+    console.log("Criança,morra!")
+    var idade20 = ("Criança,morra!")
+    }else{
+        console.log("Ancião do caralho!")
+        var idade20 = ("Ancião do caralho!")
+    }
+}
+
